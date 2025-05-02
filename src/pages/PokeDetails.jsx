22623093221d2d89658edac9b4d80130eadc360f
lozadamarkjustine <lozadamarkjustine@gmail.com>
2025-05-02 11:59:08 +0800

    update

--- src/pages/PokeDetails.jsx
+++ src/pages/PokeDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { getPokemonDetails } from '../services/pokeapi';
+import { getPokemonDetails } from '../services/pokeApi';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
