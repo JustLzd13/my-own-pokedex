070f25d2ae083c73be390e0732b65d64c4e8fd84
lozadamarkjustine <lozadamarkjustine@gmail.com>
2025-05-02 11:41:06 +0800

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PokeCard from '../components/PokeCards';
-import { getAllPokemon, getPokemonDetails } from '../services/pokeAPI';
+import { getAllPokemon, getPokemonDetails } from '../services/pokeapi';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
